ec1febd54fe0391cca058dd16a3f957bb50e3238
mirellanovroch <mirella_novroch@outlook.com>
2025-06-30 15:47:38 -0300

    Inicializa projeto

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Desafio Classificador de nível de Herói");
+const readlineSync = require('readline-sync');
+
+//Mensagem de Início
+console.log ("Bem vindo ao Desafio Classificador de Nível de Herói.")
+
+//Declaração das variáveis 
+let nomeHeroi  = readlineSync.question ("Digite seu nome: ") 
+let xpHeroi = Number(readlineSync.question ("Digte um numero entre 1.000 ate 10.001 : "))
+let nivelHeroi = " "; 
+
+//Condições 
+if (xpHeroi <= 1000){
+nivelHeroi = "Ferro"
+}else if (xpHeroi <= 2000){
+  nivelHeroi = "Bronze"
+}else if (xpHeroi <= 5000){
+  nivelHeroi = "Prata"
+}else if (xpHeroi <= 7000 ){
+  nivelHeroi = "Ouro"
+}else if (xpHeroi <= 8000 ){
+  nivelHeroi = "Platina"
+}else if (xpHeroi <= 9000  ){
+  nivelHeroi = "Ascendente"
+}else if (xpHeroi <= 10000 ){
+  nivelHeroi = "Imortal"
+}else 
+  nivelHeroi = "Radiante";
+
+
+//Saída
+console.log (`O Herói de nome ${nomeHeroi}, está no nível de ${nivelHeroi}`);
