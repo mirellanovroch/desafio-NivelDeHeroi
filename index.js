a0f652b60c6a5c7c5d4759065e8d058ab821f6c7
mirellanovroch <mirella_novroch@outlook.com>
2025-06-30 17:22:48 -0300

    feat: adiciona laço de repetição

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const readlineSync = require('readline-sync');
 
 //Mensagem de Início
-console.log ("Bem vindo ao Desafio Classificador de Nível de Herói.")
+console.log("Bem vindo ao Desafio Classificador de Nível de Herói.")
 
 //Declaração das variáveis 
-let nomeHeroi  = readlineSync.question ("Digite seu nome: ") 
-let xpHeroi = Number(readlineSync.question ("Digte um numero entre 1.000 ate 10.001 : "))
-let nivelHeroi = " "; 
+let nomeHeroi = readlineSync.question("Digite seu nome: ")
+let xpHeroi = Number(readlineSync.question("Digte um numero entre 1.000 ate 10.001 : "))
+let nivelHeroi = " ";
+
+// Laço de repetição
+do {
+  xpHeroi = Number(readlineSync.question("Digite um numero entre 1.000 ate 10.001: "));
+  if (xpHeroi < 1000 || xpHeroi > 10001) {
+    console.log("Valor inválido! Tente novamente.");
+  }
+} while (xpHeroi < 1000 || xpHeroi > 10001);
+
 
 //Condições 
-if (xpHeroi <= 1000){
-nivelHeroi = "Ferro"
-}else if (xpHeroi <= 2000){
+if (xpHeroi <= 1000) {
+  nivelHeroi = "Ferro"
+} else if (xpHeroi <= 2000) {
   nivelHeroi = "Bronze"
-}else if (xpHeroi <= 5000){
+} else if (xpHeroi <= 5000) {
   nivelHeroi = "Prata"
-}else if (xpHeroi <= 7000 ){
+} else if (xpHeroi <= 7000) {
   nivelHeroi = "Ouro"
-}else if (xpHeroi <= 8000 ){
+} else if (xpHeroi <= 8000) {
   nivelHeroi = "Platina"
-}else if (xpHeroi <= 9000  ){
+} else if (xpHeroi <= 9000) {
   nivelHeroi = "Ascendente"
-}else if (xpHeroi <= 10000 ){
+} else if (xpHeroi <= 10000) {
   nivelHeroi = "Imortal"
-}else 
+} else
   nivelHeroi = "Radiante";
 
-
 //Saída
-console.log (`O Herói de nome ${nomeHeroi}, está no nível de ${nivelHeroi}`);
+console.log(`O Herói de nome ${nomeHeroi}, está no nível de ${nivelHeroi}`);
